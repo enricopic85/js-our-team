d5c62de3f885dccda7f4ef0ca3bd6a56bb0fccbe
enricopic85 <enricodev85@gmail.com>
2022-01-10 16:23:23 +0100

    prima versione bonus

--- js/script.js
+++ js/script.js
@@ -32,40 +32,70 @@ const team = [
   ];
 
 let container=document.querySelector(".team-container");
-// for(let i=0;i<team.length;i++){
-//   let card=team[i];
-//   container.innerHTML +=`<div class="team-card">
-//     <div class="card-image">
-//       <img
-//         src="img/${card.image}"
-//         alt="${card.name}"
-//       />
-//     </div>
-//     <div class="card-text">
-//       <h3>${card.name}</h3>
-//       <p>${card.role}</p>
-//     </div>
-//   </div>`
-// }
-for(i=0;i<team.length;i++){
+for(let i=0;i<team.length;i++){
   let card=team[i];
-  let teamCard=document.createElement('div');
-  teamCard.classList.add("team-card");
-  container.append(teamCard);
-  let cardImage=document.createElement('div');
-  cardImage.classList.add("card-image");
-  teamCard.append(cardImage);
-  let img=document.createElement("img");
-  img.src=`img/${card.image}`;
-  img.alt=card.name;
-  cardImage.append(img);
-  let cardText=document.createElement("div");
-  cardText.classList.add("card-text");
-  teamCard.append(cardText);
-  let nameCard=document.createElement("h3");
-  nameCard.innerText=card.name;
-  cardText.append(nameCard);
-  let roleCard=document.createElement("p");
-  roleCard.innerText=card.role;
-  cardText.append(roleCard);
+  container.innerHTML +=`<div class="team-card">
+    <div class="card-image">
+      <img
+        src="img/${card.image}"
+        alt="${card.name}"
+      />
+    </div>
+    <div class="card-text">
+      <h3>${card.name}</h3>
+      <p>${card.role}</p>
+    </div>
+  </div>`
 }
+// for(i=0;i<team.length;i++){
+//   let card=team[i];
+//   let teamCard=document.createElement('div');
+//   teamCard.classList.add("team-card");
+//   container.append(teamCard);
+//   let cardImage=document.createElement('div');
+//   cardImage.classList.add("card-image");
+//   teamCard.append(cardImage);
+//   let img=document.createElement("img");
+//   img.src=`img/${card.image}`;
+//   img.alt=card.name;
+//   cardImage.append(img);
+//   let cardText=document.createElement("div");
+//   cardText.classList.add("card-text");
+//   teamCard.append(cardText);
+//   let nameCard=document.createElement("h3");
+//   nameCard.innerText=card.name;
+//   cardText.append(nameCard);
+//   let roleCard=document.createElement("p");
+//   roleCard.innerText=card.role;
+//   cardText.append(roleCard);
+// }
+let addCard=document.getElementById("addMemberButton");
+addCard.addEventListener('click',function(){
+      let newName=document.getElementById("name").value;
+      let newRole=document.getElementById("role").value;
+      let newImg=document.getElementById("image").value;
+      let newObject={
+        name: newName,
+        role: newRole,
+        image: newImg
+      }
+      team.push(newObject);
+})
+// program to append an object to an array
+
+// function insertObject(arr, obj) {
+
+//   // append object
+//   arr.push(obj);
+  
+//   console.log(arr);
+// }
+
+// // original array
+// let array = [1, 2, 3];
+
+// // object to add
+// let object = {x: 12, y: 8};
+
+// // call the function
+// console.log(insertObject(array, object));
